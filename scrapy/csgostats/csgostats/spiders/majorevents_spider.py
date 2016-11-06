92e7ac8daa6b398dab34db8960c26496ba67217f
Bastien Sebire <bastien@glitchr-studio.com>
2016-11-06 21:15:00 +0100

    Team info crawler reworked and now works - scraped data are now inserted in the DB when the server starts (majorEvents & teamInfo) - Table added to display teamInfo

--- scrapy/csgostats/csgostats/spiders/majorevents_spider.py
+++ scrapy/csgostats/csgostats/spiders/majorevents_spider.py
@@ -15,12 +15,16 @@ class MajorEventsSpider(scrapy.Spider):
 
     def parse(self, response):
         for event in response.css('.table-bordered tr'):
+            #if not null, if CSGO event
 
-            if event.css('td a::text').extract():
-    			item = MajorEventItem()
-    			item['name'] = event.css('td a::text').extract()
-    			item['prizepool'] = event.css('td:nth-child(6)::text').extract()
-    			item['location'] = event.css('td:nth-child(8)::text').extract()
-    			item['winner'] = event.css('td:nth-child(9) span span:nth-child(2) a::text').extract()
-    			item['runnerUp'] = event.css('td:nth-child(10) span span:nth-child(2) a::text').extract()
-    			yield item
+            if event.css('td:nth-child(3) a::text').extract() and "Csgo" in str(event.css('td:nth-child(4) a::attr(href)').extract()):
+                item = MajorEventItem()
+                item['name'] = event.css('td:nth-child(3) a::text').extract()
+                item['prizepool'] = event.css('td:nth-child(6)::text').extract()
+                item['location'] = event.css('td:nth-child(8)::text').extract()
+                item['winner'] = event.css('td:nth-child(9) span span:nth-child(2) a::text').extract()
+                item['runnerUp'] = event.css('td:nth-child(10) span span:nth-child(2) a::text').extract()
+
+                #if winner is determinated
+                if item['winner']:
+                    yield item
--- serveur.js
+++ serveur.js
@@ -13,7 +13,7 @@ var fs = require('fs');
 var assert = require('assert');
 
 var majorevents;
-var teams;
+var teamInfo;
 
 // Mongoose Schemas
 
@@ -25,46 +25,100 @@ var majorEventSchema = mongoose.Schema({
     runnerUp: String
 });
 
+var teamInfoSchema = mongoose.Schema({
+    teamname: String,
+    country: String,
+    totalEarning: String,
+    logo: String
+});
+
 var MajorEvent = mongoose.model('MajorEvent', majorEventSchema);
+var TeamInfo = mongoose.model('TeamInfo', teamInfoSchema);
 
 // crawl major events
 exec('scrapy crawl majorevents -t json --nolog -o - > "majorevents.json"', {
  cwd: 'scrapy/csgostats'
  }, function(error, stdout, stderr) {
   majorevents = require('./scrapy/csgostats/majorevents.json');
-  console.log(majorevents);
  });
 
 //crawl teams list
  exec('scrapy crawl teamname -t json --nolog -o - > "teamname.json"', {
   cwd: 'scrapy/csgostats'
   }, function(error, stdout, stderr) {
-   teams = require('./scrapy/csgostats/teamname.json');
-   console.log(teams);
+   exec('scrapy crawl teaminfo -t json --nolog -o - > "teaminfo.json"', {
+    cwd: 'scrapy/csgostats'
+    }, function(error, stdout, stderr) {
+     teamInfo = require('./scrapy/csgostats/teaminfo.json');
+     insertDataInDb();
   });
-
-//Mongoose
-mongoose.connect('mongodb://localhost/CSGO_STATS');
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    console.log('connected to CSGO_STATS');
-
-    app.get('/', function (req, res) {
-            res.render('index.ejs', {events: majorevents});
-        })
-        .get('/about', function (req, res) {
-            res.render('about.ejs');
-        })
-        .get('/teams', function(req, res){
-            res.render('teams.ejs', {teams: teams});
-        })
-        .use("/css", express.static(__dirname + "/css"))
-        .use("*", function (req, res) {
-            res.render("404.ejs");
-        })
 });
+
+app.get('/', function (req, res) {
+        res.render('index.ejs', {events: majorevents});
+        getMajorEvent();
+    })
+    .get('/about', function (req, res) {
+        res.render('about.ejs');
+    })
+    .get('/teams', function(req, res){
+        res.render('teams.ejs', {teams: teamInfo});
+    })
+    .use("/css", express.static(__dirname + "/css"))
+    .use("*", function (req, res) {
+        res.render("404.ejs");
+    })
 
 server.listen(8080);
 console.log('Listening at localhost:8080');
+
+function insertDataInDb() 
+{
+  mongoose.connect('mongodb://localhost/CSGO_STATS');
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function () 
+  {
+      for(var i = 0; i < majorevents.length; i++)
+      {
+        var event = majorevents[i];
+        var item = new MajorEvent({ name: event.name, location: event.location, prizepool: event.prizepool, winner: event.winner, runnerUp: event.runnerUp, upsert: true });
+        MajorEvent.update({name: event.name}, item, {upsert: true}, function (err) {});
+      }
+
+      for(var i = 0; i < teamInfo.length; i++)
+      {
+        var team = teamInfo[i];
+        var item = new TeamInfo({ teamname: team.teamname, country: team.country, totalEarning: team.totalEarning, logo: team.logo, upsert: true });
+        TeamInfo.update({teamname: event.teamname}, item, {upsert: true}, function (err) {});
+      }
+  });
+}
+
+function getMajorEvent()
+{
+  mongoose.connect('mongodb://localhost/CSGO_STATS');
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function () 
+  {
+      MajorEvent.find({}, function(err, events) {
+          //return events;
+          console.log(events);
+      });
+  });
+}
+
+function getTeamInfo()
+{
+  mongoose.connect('mongodb://localhost/CSGO_STATS');
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function () 
+  {
+      TeamInfo.find({}, function(err, teams) {
+          //return teams;
+          console.log(teams);
+      });
+  });
+}
